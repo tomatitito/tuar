8202c67970756d169a61686deb43647ec9a5c756
Jens Kouros <jens.kouros@posteo.de>
2019-12-31 15:29:54 +0100

    Add main.tf with provider and instance and README

---
+++ 02-introduction/one-webserver/README.md
@@ -0,0 +1,3 @@
+# Deploying a web server
+
+This is still a single EC2 Instance, but it can be acessed via the internet. Therefore it needs a security group.
---
+++ 02-introduction/one-webserver/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-40d28157"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #! /bin/bash
+              echo "Hello, World!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
